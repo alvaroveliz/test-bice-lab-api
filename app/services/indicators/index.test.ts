132926575ee315dc6e82e2ba13273d0d04932601
Álvaro Véliz <alvaro.veliz@gmail.com>
2020-06-01 17:07:58 -0400

    adding testing

--- app/services/indicators/index.test.ts
+++ app/services/indicators/index.test.ts
@@ -2,7 +2,17 @@ import IndicatorsService from '../indicators';
 
 const service = new IndicatorsService();
 
-test('service is showing last data', async () => {
+test('service is showing the indicators', async () => {
+    const expectedKeys = ['cobre', 'dolar', 'euro', 'ipc', 'ivp', 'oro', 'plata', 'uf', 'utm', 'yen'];
     const data = await service.getLast();
-    expect(data).toBe('peanut butter');
+    const keys = Object.keys(data);
+    expect(keys).toEqual(expectedKeys);
+});
+
+test('service is showing correct structure for "cobre"', async () => {
+    const testKey = 'cobre';
+    const expectedKeys = ['key', 'name', 'unit', 'values'];
+    const data = await service.getValuesByKey(testKey);
+    const keys = Object.keys(data);
+    expect(keys).toEqual(expectedKeys);
 });
